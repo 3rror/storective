b838dc59bc337b6b1c9a3942f1becbf3aab6fbcb
Gianluca Andreotti <gianlucaatlas (at) gmail (com)>
2014-11-22 16:07:30 +0100

    Improved tests and code

--- lib/storective.rb
+++ lib/storective.rb
@@ -9,12 +9,24 @@ class Storective
 
   attr_accessor :settings
 
-  def initialize(settings = {})
-    @settings = settings
+  def initialize(options = {})
+    @settings = options
   end
 
+  def search_for(term)
+    options = @settings.merge({ term: term })
+    self.class.get('/search', query: options)['results']
+  end
+
+  def enable_debug(console = $stdout)
+    self.class.debug_output(console)
+  end
+
+  private
+
   def method_missing(meth, *args, &blk)
-    if param_permitted? meth
+    # Check if meth is a settable option
+    if allowed_params? meth
       if args.empty?
         @settings[meth]
       else
@@ -26,18 +38,7 @@ class Storective
     end
   end
 
-  def search_for(term)
-    options = @settings.merge({ term: term })
-    self.class.get('/search', query: options)['results']
-  end
-
-  def enable_debug
-    self.class.debug_output $stdout
-  end
-
-  private
-
-  def param_permitted?(param)
+  def allowed_params?(param)
     allowed_params = [:term, :country, :media, :entity, :attribute,
                       :callback, :limit, :lang, :version, :explicit]
     allowed_params.include? param
--- test/test_storective.rb
+++ test/test_storective.rb
@@ -2,23 +2,49 @@ require 'minitest/autorun'
 require 'storective'
 
 describe Storective do
+  let(:default_store) { Storective.new }
 
-  before do
-    @store = Storective.new
-  end
-
-  let(:dflt_storective) { Storective.new }
-
-  describe 'when asked about a term' do
-    it 'must return an array with results' do
-      @store.search_for('test').must_be_kind_of Array
+  describe 'when you want to search for a term' do
+    it 'must return an array with the results' do
+      skip # because it can really slow down tests
+      default_store.search_for('test').must_be_kind_of Array
     end
   end
 
-  describe 'when you want to limit your search option' do
-    it 'must allow to set the country code of the store' do
-      @store.country 'IT'
-      @store.country.must_equal('IT')
+  describe 'when you want to set an option for your search' do
+    it 'must allow you to do it with method-chain' do
+      default_store.country('IT').limit(1).must_be_instance_of Storective
+    end
+
+    it 'must allow you to do it during the initialization' do
+      Storective.new country: 'IT', limit: 1
+    end
+  end
+
+  it 'must allow you to get a previously setted option' do
+    default_store.country 'IT'
+    default_store.country.must_equal('IT')
+  end
+
+  describe 'when you want to search by ID' do
+    it "must allow you to specify if it's the artist ID" do
+      skip
+      default_store.search_artist_by_ID 909253
+    end
+
+    it "must allow you to specify if it's the album ID" do
+      skip
+      default_store.search_album_by_ID 120954021
+    end
+
+    it "must allow you to specify if it's the collection ID" do
+      skip
+      default_store.search_collection_by_ID 120954021
+    end
+
+    it "must allow you to specify if it's the track ID" do
+      skip
+      default_store.search_track_by_ID 120954021
     end
   end
 end

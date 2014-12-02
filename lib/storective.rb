ab643532f8a4e3799bc3d03cf528da0ec332723f
Gianluca Andreotti <gianlucaatlas (at) gmail (com)>
2014-12-02 15:41:16 +0100

    Using ActiveSupport to "rubify" results.

--- lib/storective.rb
+++ lib/storective.rb
@@ -1,6 +1,7 @@
 require 'storective/version'
 require 'net/http'
 require 'json'
+require 'active_support/core_ext/hash'
 
 class Storective
   attr_reader :settings
@@ -14,7 +15,11 @@ class Storective
     uri = URI('https://itunes.apple.com/search')
     uri.query = URI.encode_www_form(all_params)
     res = Net::HTTP.get_response(uri)
-    JSON.parse(res.body)['results']
+    parsed_res = JSON.parse(res.body)['results']
+
+    parsed_res.map do |result|
+      result.transform_keys { |key| key.underscore.to_sym}
+    end
   end
 
   allowed_params = [:term, :country, :media, :entity, :attribute,
